9bf7bdbf6a274f3943acf339d8483e681df625cf
Anand <nairanand.delxn@gmail.com>
2025-01-29 16:59:22 +0530

    html css

---
+++ script.js
@@ -0,0 +1,33 @@
+
+        // function submitForm() {
+        //     let name = document.getElementById("name").value.trim();
+        //     let email = document.getElementById("email").value.trim();
+        //     let message = document.getElementById("message").value.trim();
+
+        //     let nameError = document.getElementById("nameError");
+        //     let emailError = document.getElementById("emailError");
+        //     let messageError = document.getElementById("messageError");
+
+        //     nameError.innerText = "";
+        //     emailError.innerText = "";
+        //     messageError.innerText = "";
+
+        //     if (name === "") {
+        //         nameError.innerText = "Name is required!";
+        //         return;
+        //     }
+        //     if (email === "") {
+        //         emailError.innerText = "Email is required!";
+        //         return;
+        //     } else if (!email.includes("@")) {
+        //         emailError.innerText = "Enter a valid email!";
+        //         return;
+        //     }
+        //     if (message === "") {
+        //         messageError.innerText = "Message cannot be empty!";
+        //         return;
+        //     }
+
+        //     alert("Form submitted successfully!");
+        //     document.getElementById("contactForm").reset();
+        // }
